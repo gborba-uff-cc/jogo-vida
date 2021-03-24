256544382fcb5b958c0f2a16dd2e814d7e303f8a
Gabriel Borba <gborba@id.uff.br>
2021-03-23 23:23:09 -0300

    comentarios marcando o que é preciso fazer

--- app/Main.hs
+++ app/Main.hs
@@ -4,3 +4,20 @@ import Lib
 
 main :: IO ()
 main = someFunc
+
+{- SECTION - Entrada e Saida
+NOTE - pegar o tabuleiro definido pelo usuario
+NOTE - pegar o numero maximo de iteracoes desejadas
+NOTE - (1) as iteracoes terminam ao chegar no maximo desejado; OU
+NOTE - (2) as iteracoes terminam quando não ocorrerem mais alteracoes no tabuleiro
+NOTE - apresentar o tabuleiro ao fim das iteracoes
+TODO - Entrada e Saida
+!SECTION -}
+{- SECTION - Regras
+NOTE - reproducao     - morta -> viva : =3 celulas vivas adjacentes
+NOTE - infeccao       - viva -> zumbi : >0 celulas zumbi adj.
+NOTE - subpopulacao   - viva -> morta : <2 celulas vivas adj. e 0 zumbi adj.
+NOTE - superpopulacao - viva -> morta : >3 celulas vivas adj. e 0 zumbi adj.
+NOTE - inanicao       - zumbi -> morta : =0 celulas vivas adj.
+TODO - Regras
+!SECTION -}
