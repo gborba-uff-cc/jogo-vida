c57dff04e7498ad6697f0a2f54a58b910418565e
Gabriel Borba <gborba@id.uff.br>
2021-04-09 09:17:43 -0300

    funcao dedicada a gerar o padrão de vizinhos de uma posicao

--- Main.hs
+++ Main.hs
@@ -27,6 +27,12 @@ posicaoValida t (x,y) =
     0 <= x && x <= larguraTabuleiro t &&
     0 <= y && y <= alturaTabuleiro t
 
+possiveisPosicoesVizinhas :: Posicao -> [Posicao]
+possiveisPosicoesVizinhas (x, y) =
+    [(x-1,y-1), (x, y-1), (x+1, y-1),
+     (x-1,y) {-,(x, y)-}, (x+1,y)   ,
+     (x-1,y+1), (x, y+1), (x+1, y+1)]
+
 restringePosicao :: Int -> Int -> Posicao -> Posicao
 restringePosicao valMaxX valMaxY (x, y) = (x `mod` valMaxX, y `mod` valMaxY)
 
