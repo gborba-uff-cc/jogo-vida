bdf0eff34f543df9d77ce072a9b7abbefbf12048
Gabriel Borba <gborba@id.uff.br>
2021-04-09 09:20:58 -0300

    posicoesVizinhas usa a nova funcao possiveisPosicoesVizinhas

--- Main.hs
+++ Main.hs
@@ -39,10 +39,7 @@ restringePosicao valMaxX valMaxY (x, y) = (x `mod` valMaxX, y `mod` valMaxY)
 posicoesVizinhas :: Tabuleiro -> Posicao -> [Posicao]
 posicoesVizinhas Tabuleiro {larguraTabuleiro=l,alturaTabuleiro=a} (x, y) =
     -- delete((x,y), listaSemRepeticao(listaVizinhos))
-    Data.List.delete (x,y) $ Data.List.nub $ map (restringePosicao l a)
-        [(x-1,y-1), (x, y-1), (x+1, y-1),
-         (x-1,y) ,{-(x, y),-} (x+1,y)   ,
-         (x-1,y+1), (x, y+1), (x+1, y+1)]
+    Data.List.delete (x,y) $ Data.List.nub $ map (restringePosicao l a) $ possiveisPosicoesVizinhas (x,y)
 
 contarElemento :: Eq a => a -> [a] -> Int
 contarElemento e [] = 0
